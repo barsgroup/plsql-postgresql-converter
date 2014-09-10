060315885516e0667d2edb7b44e0435c8bc9c0e2
Dmitry Kalyanov <Kalyanov.Dmitry@gmail.com>
2014-09-10 14:09:52 +0400

    Копировать также PLSQLParser.tokens при сборке парсера

--- plsql-parser/lexer-parser-walker/build_command.sh
+++ plsql-parser/lexer-parser-walker/build_command.sh
@@ -1,1 +1,4 @@
-(cd .. && mvn clean validate) && mvn package && cp target/lexer-parser-walker-1.0-SNAPSHOT.jar ../../eclipse-workspace-parser/parser/lib/
+(cd .. && mvn clean validate) &&
+mvn package &&
+cp target/lexer-parser-walker-1.0-SNAPSHOT.jar ../../eclipse-workspace-parser/parser/lib/ &&
+cp target/generated-sources/antlr3/br/com/porcelli/parser/plsql/PLSQLParser.tokens ../../eclipse-workspace-parser/parser/antlr-source
