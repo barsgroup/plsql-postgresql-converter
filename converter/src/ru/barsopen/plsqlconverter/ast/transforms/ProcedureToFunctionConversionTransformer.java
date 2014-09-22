37a60e0f2c84e8b2173113cafb468dcca26388b2
Dmitry Kalyanov <Kalyanov.Dmitry@gmail.com>
2014-09-22 18:32:38 +0400

    Поправил трансформацию function->function

--- converter/src/ru/barsopen/plsqlconverter/ast/transforms/ProcedureToFunctionConversionTransformer.java
+++ converter/src/ru/barsopen/plsqlconverter/ast/transforms/ProcedureToFunctionConversionTransformer.java
@@ -94,9 +94,18 @@ public class ProcedureToFunctionConversionTransformer {
 	}
 
 	private void transformFunc() {
+		create_function_body func = (create_function_body)proc_or_func;
+		List<Integer> in_params = new ArrayList<Integer>();
+		List<Integer> out_params = new ArrayList<Integer>();
+		classifyParameters(func.parameters, in_params, out_params);
+		if (out_params.size() > 0) {
+			transformFuncCallSites(in_params, out_params);
+			addFuncPrimaryRetval(func);
+			transformFuncReturns(func);
+		}
 
 		/*
-		type_spec returnTypeSpec = computeReturnTypeSpec(proc);
+		
 		create_function_body func = parser.make_create_function_body(
 				AstUtil.createAstNode(PLSQLParser.SQL92_RESERVED_CREATE),
 				AstUtil.createAstNode(PLSQLParser.REPLACE_VK),
@@ -111,6 +120,177 @@ public class ProcedureToFunctionConversionTransformer {
 				(function_impl)proc.create_procedure_body_impl);
 		
 		proc._getParent()._replace(proc, func);*/
+	}
+
+	private void transformFuncReturns(create_function_body func) {
+		List<return_statement> statements = AstUtil.getDescendantsOfType(func, return_statement.class);
+		for (return_statement rs: statements) {
+			seq_of_statements parent = (seq_of_statements)rs._getParent();
+			expression re = rs.expression;
+			rs.set_expression(null);
+			int idx = parent.stat_or_labels.indexOf(rs);
+			parent.insert_stat_or_labels(
+				idx,
+				parser.make_assignment_statement(
+					parser.make_general_element(
+						Arrays.asList(
+							(general_element_item)parser.make_general_element_id(parser.make_id("__retval__"))
+						)
+					),
+					re
+				)
+			);
+		}
+	}
+
+	private void addFuncPrimaryRetval(create_function_body func) {
+		type_spec returnTypeSpec = parser.make_type_spec_custom(parser.make_type_name(Arrays.asList(parser.make_id("record"))), null, null);
+		type_spec paramTypeSpec = func.type_spec;
+		func.set_type_spec(returnTypeSpec);
+		func.parameters.add_parameters(
+			parser.make_parameter(
+				parser.make_parameter_name(null, parser.make_id("__retval__")),
+				Arrays.asList(
+					(parameter_dir_spec)parser.make_parameter_out()
+				),
+				paramTypeSpec,
+				null
+			)
+		);
+	}
+
+	private void transformFuncCallSites(List<Integer> in_params, List<Integer> out_params) {
+		List<general_element_item> references = sa.defToScopeEntry.get(proc_or_func).references;
+		List<parameter> formalArgs = (((create_function_body)proc_or_func).parameters).parameters;
+		Map<String, Integer> name2pos = new HashMap<String, Integer>();
+		{
+			int i = 0;
+			for (parameter p: formalArgs) {
+				name2pos.put(AstUtil.normalizeId(p.parameter_name.id.value), i);
+				++i;
+			}
+		}
+		for (general_element_item refItem: references) {
+			general_element_id refItemId = (general_element_id)refItem;
+			general_element callSite = (general_element)refItem._getParent();
+			if (!canTransformFuncCallSite(callSite)) {
+				System.err.printf("Can't transform function %s call site at %d:%d\n", refItemId.id.value, refItemId._line, refItemId._col);
+				continue;
+			}
+
+			int idx = callSite.general_element_items.indexOf(refItem);
+			if (idx == callSite.general_element_items.size() - 1 || !(callSite.general_element_items.get(idx + 1) instanceof function_argument)) {
+				callSite.insert_general_element_items(idx + 1, parser.make_function_argument(null));
+			}
+			function_argument argsNode = (function_argument)callSite.general_element_items.get(idx + 1);
+			Map<String, argument> passedArgs = new HashMap<String, argument>();
+			List<argument> deletedArgs = new ArrayList<argument>();
+			{
+				int i = 0;
+				for (argument arg: argsNode.arguments) {
+					String name;
+					if (arg.is_parameter_name()) {
+						name = AstUtil.normalizeId(arg.parameter_name.id.value);
+					} else {
+						name = AstUtil.normalizeId(formalArgs.get(i).parameter_name.id.value);
+						++i;
+					}
+					passedArgs.put(name, arg);
+					if (!in_params.contains(name2pos.get(name))) {
+						deletedArgs.add(arg);
+					}
+				}
+			}
+			for (argument arg: deletedArgs) {
+				argsNode.remove_arguments(arg);
+				MiscConversionsTransformer.reattachCommentsFromDeletedNodes(argsNode, arg);
+			}
+
+			assignment_statement assignment = parser.make_assignment_statement(
+				parser.make_general_element(
+					Arrays.asList(
+						(general_element_item)parser.make_general_element_id(parser.make_id("__tmp__"))
+					)
+				),
+				null 
+			);
+			List<stat_or_label> statements = new ArrayList<stat_or_label>();
+			statements.add(assignment);
+			for (int out_param_no: out_params) {
+				parameter formalArg = formalArgs.get(out_param_no);
+				argument outArg = passedArgs.get(AstUtil.normalizeId(formalArg.parameter_name.id.value));
+				general_element outArgExpr = (general_element)((general_expression)outArg.expression).expression_element;
+				outArgExpr = parser.parsegeneral_element(AstUtil.cloneTree(outArgExpr.unparse()));
+				assignment_statement unwrapArgStatement = parser.make_assignment_statement(
+					outArgExpr,
+					parser.make_general_expression(
+						parser.make_general_element(
+							Arrays.asList(
+								(general_element_item)parser.make_general_element_id(parser.make_id("__tmp__")),
+								(general_element_item)parser.make_general_element_id(parser.make_id(formalArg.parameter_name.id.value))
+							)
+						)
+					)
+				);
+				statements.add(unwrapArgStatement);
+			}
+			if (isCallSiteInAssignment(callSite)) {
+				assignment_statement ownerAssignment = (assignment_statement)callSite._getParent()._getParent();
+				assignment_target at = ownerAssignment.assignment_target;
+				ownerAssignment.set_assignment_target(null);
+
+				assignment_statement unwrapArgStatement = parser.make_assignment_statement(
+					at,
+					parser.make_general_expression(
+						parser.make_general_element(
+							Arrays.asList(
+								(general_element_item)parser.make_general_element_id(parser.make_id("__tmp__")),
+								(general_element_item)parser.make_general_element_id(parser.make_id("__retval__"))
+							)
+						)
+					)
+				);
+				statements.add(unwrapArgStatement);
+			}
+			block replacement = parser.make_block(
+				Arrays.asList(
+					(declare_spec)parser.make_variable_declaration(
+						parser.make_variable_name(null, Arrays.asList(parser.make_id("__tmp__"))),
+						parser.make_type_spec_custom(parser.make_type_name(Arrays.asList(parser.make_id("record"))), null, null),
+						null,
+						null,
+						null
+					)
+				),
+				parser.make_body(
+					null,
+					parser.make_seq_of_statements(
+						statements
+					),
+					null
+				)
+			);
+			_baseNode whatToReplace = isCallSiteInSeq(callSite) ? callSite : callSite._getParent()._getParent();
+			whatToReplace._getParent()._replace(whatToReplace, replacement);
+			assignment.set_expression(parser.make_general_expression(callSite));
+		}
+	}
+
+	private boolean canTransformFuncCallSite(general_element callSite) {
+		boolean isInAssignment = isCallSiteInAssignment(callSite);
+		boolean isInSeq = isCallSiteInSeq(callSite);
+		return isInAssignment || isInSeq;
+	}
+
+	public boolean isCallSiteInSeq(general_element callSite) {
+		boolean isInSeq = callSite._getParent() instanceof seq_of_statements || callSite._getParent() instanceof labeled_statement;
+		return isInSeq;
+	}
+
+	public boolean isCallSiteInAssignment(general_element callSite) {
+		boolean isInAssignment = callSite._getParent() instanceof general_expression
+				&& callSite._getParent()._getParent() instanceof assignment_statement;
+		return isInAssignment;
 	}
 
 	private void transformProc() {
